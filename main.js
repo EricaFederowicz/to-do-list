874fe0f5558e86fb626636f55a7cb55f701276e4
EricaFederowicz <132520258+EricaFederowicz@users.noreply.github.com>
2023-12-03 16:17:52 -0300

    updates
    complete button clicked style and previous tasks storage

--- main.js
+++ main.js
@@ -1,37 +1,78 @@
-
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     const inputBox = document.getElementById('input-box');
     const addButton = document.querySelector('.add-button');
     const taskList = document.getElementById('task-list');
-
-    addButton.addEventListener('click', function() {
-        const taskName = document.getElementById('input-box').value;
-
-        if (taskName !== '') {
-            const listItem = document.createElement('li');
-            listItem.className = 'list-item';
-        
-
-        listItem.innerHTML = `
-            <button class="complete-button">
-                <img src="img/check.png">
-            </button>
-            <section class="task">
-                <span class="task-name">${taskName}</span>
-            </section>
-        `;
-
-        taskList.appendChild(listItem);
-
+  
+    const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    renderTasks(storedTasks);
+  
+    addButton.addEventListener('click', function () {
+      const taskName = inputBox.value.trim();
+  
+      if (taskName !== '') {
+        const newTask = {
+          name: taskName,
+          completed: true
+        };
+  
+        storedTasks.push(newTask);
+        localStorage.setItem('tasks', JSON.stringify(storedTasks));
+  
+        renderTasks(storedTasks);
+  
         inputBox.value = '';
+      }
+    });
+  
+    taskList.addEventListener('click', function (event) {
+      const target = event.target;
+  
+      if (target.classList.contains('complete-button')) {
+        const listItem = target.closest('.list-item');
+        const index = Array.from(listItem.parentNode.children).indexOf(listItem);
+  
+        storedTasks[index].completed = !storedTasks[index].completed;
+        localStorage.setItem('tasks', JSON.stringify(storedTasks));
+  
+        renderTasks(storedTasks);
+      }
+    });
+  
+    function renderTasks(tasks) {
+      taskList.innerHTML = '';
+  
+      tasks.forEach(function (task, index) {
+        const listItem = document.createElement('li');
+        listItem.className = 'list-item';
+  
+        listItem.innerHTML = `
+          <button class="complete-button ${task.completed ? 'marked' : ''}">
+            <img src="img/check.png">
+          </button>
+          <section class="task ${task.completed ? 'marked' : ''}">
+            <span class="task-name">${task.name}</span>
+          </section>
+        `;
+  
+        taskList.appendChild(listItem);
+      });
+
+      applyStylesToDynamicButtons();
     }
-});
+
+    function applyStylesToDynamicButtons() {
+        const completeButtons = document.getElementsByClassName('complete-button');
+
+        for (const button of completeButtons) {
+            
+            button.addEventListener('click', function() {
+                this.classList.toggle('marked');
+            });
+        }
+    }
+
+    applyStylesToDynamicButtons();
 });
 
-const completeButtons = document.getElementsByClassName('complete-button');
 
-for (const button of completeButtons) {
-    button.addEventListener('click', function(){
-        this.classList.toggle('marked');
-    })
-};
+  
